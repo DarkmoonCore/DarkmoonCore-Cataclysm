6b4141506d6b826184273e9d434dbea2c9b68c37
War-King <darkmoon.wow@abv.bg>
2011-09-30 22:25:04 +0300

    [1] Basic code of DarkmoonCore uploaded and the start of the project given. Congratulations.

---
+++ revision.h.in.cmake
@@ -0,0 +1,11 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _HASH                      "@rev_hash@"
+ #define _DATE                      "@rev_date@"
+ #define VER_COMPANYNAME_STR        "DarkmoonCore Developers"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2011 DarkmoonCore"
+ #define VER_FILEVERSION            0,0,0
+ #define VER_FILEVERSION_STR        "@rev_date@ (@rev_hash@)"
+ #define VER_PRODUCTVERSION         VER_FILEVERSION
+ #define VER_PRODUCTVERSION_STR     "01"
+#endif // __REVISION_H__
